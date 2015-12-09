8ea0a39937eee7b3dd83b999ff3cdcdf3b6eeb86
aunz <aunz10@yahoo.com>
2015-12-10 00:00:22 +1300

    Remove unused files: browserSync and injectLoader

--- template/expressServer.js
+++ template/expressServer.js
@@ -9,7 +9,7 @@ app.disable('x-powered-by')
 app.use(compression())
 app.use(express.static('./build/client'))  
  
-app.listen(process.env.port || 3000, function() {
+app.listen(process.env.PORT || 3000 ,process.env.HOST, function() {
 	console.log('Express app listening at http://%s:%s', this.address().address, this.address().port)
   console.log('NODE_ENV:',process.env.NODE_ENV,require('path').resolve(),__dirname)
 })
