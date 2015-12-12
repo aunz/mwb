21f164c25c274c3e811a184f79bd04c444b17d61
aunz <aunz10@yahoo.com>
2015-12-12 22:43:30 +1300

    refactory mwb, move to bin folder, move ascii to bin

--- test/index.js
+++ test/index.js
@@ -1,1 +1,13 @@
-'use strict'
+import {expect} from 'chai'
+
+import f from '../kaka.js'
+
+describe('Hello', () => {
+  
+	it('should time  2', () => {
+		
+		expect(f(2)).to.equal(20)
+		// expect(0).to.not.be.false;
+	});
+
+});
